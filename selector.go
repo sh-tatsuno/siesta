0c2677d06821555eae02acd8225a374d58a9d4d1
sh-tatsuno <wasabi.mandarin@gmail.com>
2019-04-01 22:23:54 +0900

    feat: add selector

---
+++ selector.go
@@ -0,0 +1,14 @@
+package siesta
+
+import (
+	"github.com/sclevine/agouti"
+)
+
+func Selector(target string) *agouti.Selection {
+	// TODO: conditional in target sentence
+	// linkText
+	// css
+	// name
+	// xpath
+	return new(agouti.Selection)
+}
